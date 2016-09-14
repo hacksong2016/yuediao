5fe3eb7e9b7a2eec716974f50a84a56ac582eff9
fanjingyuan <fanjingyuan@fami2u.com>
2016-09-14 18:13:39 +0800

    yuediao

---
+++ DateForFishing/XTThirdTableViewCell.h
@@ -0,0 +1,27 @@
+//
+//  XTThirdTableViewCell.h
+//  DateForFishing
+//
+//  Created by ZhengXiangteng on 16/9/8.
+//  Copyright © 2016年 前辈丶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Comment.h"
+@protocol XTThirdTableViewCellDelegate <NSObject>
+
+- (void)thirdTableViewCellsearcimage:(UIButton *)button;
+
+- (void)thirdTableViewCellbutton:(UIButton *)button;
+
+@end
+@interface XTThirdTableViewCell : UITableViewCell
+
++ (instancetype)cellWithTableView:(UITableView *)tableView;
+
+/** comment模型 */
+@property (nonatomic, strong) Comment *comment;
+
+@property (nonatomic, weak) id<XTThirdTableViewCellDelegate> delegate;
+
+@end
